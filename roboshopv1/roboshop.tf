5f775c6535703af9e6e79d4dc5d687e47a849542
svati <svativangapandu9@gmail.com>
2023-11-17 14:35:53 +0530

    teraform

--- roboshopv1/roboshop.tf
+++ roboshopv1/roboshop.tf
@@ -28,7 +28,7 @@ resource "aws_instance" "cart" {
 resource "aws_instance" "catalogue" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.small"
-
+  vpc_security_group_ids = [''sg-041edd0364521caf3'']
   tags = {
     Name = "catalogue"
   }
