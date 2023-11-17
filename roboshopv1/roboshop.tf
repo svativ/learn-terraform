857d3afdc7119ab1b6d8f249d2dd594310efe7f3
svati <svativangapandu9@gmail.com>
2023-11-17 15:27:25 +0530

    teraform

--- roboshopv1/roboshop.tf
+++ roboshopv1/roboshop.tf
@@ -1,98 +1,182 @@
 resource "aws_instance" "frontend" {
-  ami           = "ami-03265a0778a880afb"
-  instance_type = "t3.small"
+  ami                    = ami-03265a0778a880afb
+  instance_type          = "t3.small"
+  vpc_security_group_ids = ["sg-041edd0364521caf3"]
 
   tags = {
     Name = "frontend"
   }
 }
 
+resource "aws_route53_record" "frontend" {
+  zone_id = Z0018290WARH7MQ8A27A
+  name    = "frontend-dev.sdevops99.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
+}
+
 resource "aws_instance" "mongodb" {
-  ami           = "ami-03265a0778a880afb"
-  instance_type = "t3.small"
+  ami                    = ami-03265a0778a880afb
+  instance_type          = "t3.small"
+  vpc_security_group_ids = ["sg-041edd0364521caf3"]
 
   tags = {
     Name = "mongodb"
   }
 }
 
+resource "aws_route53_record" "mongodb" {
+  zone_id = Z0018290WARH7MQ8A27A
+  name    = "mongodb-dev.sdevops99.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mongodb.private_ip]
+}
+
+resource "aws_instance" "catalogue" {
+  ami                    = ami-03265a0778a880afb
+  instance_type          = "t3.small"
+  vpc_security_group_ids = ["sg-041edd0364521caf3"]
+
+  tags = {
+    Name = "catalogue"
+  }
+}
+
+resource "aws_route53_record" "catalogue" {
+  zone_id = Z0018290WARH7MQ8A27A
+  name    = "catalogue-dev.sdevops99.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.catalogue.private_ip]
+}
+
 resource "aws_instance" "cart" {
-  ami           = "ami-03265a0778a880afb"
-  instance_type = "t3.small"
+  ami                    = ami-03265a0778a880afb
+  instance_type          = "t3.small"
+  vpc_security_group_ids = ["sg-041edd0364521caf3"]
 
   tags = {
     Name = "cart"
   }
 }
 
-resource "aws_instance" "catalogue" {
-  ami           = "ami-03265a0778a880afb"
-  instance_type = "t3.small"
-  vpc_security_group_ids = [''sg-041edd0364521caf3'']
-  tags = {
-    Name = "catalogue"
-  }
+resource "aws_route53_record" "cart" {
+  zone_id = Z0018290WARH7MQ8A27A
+  name    = "cart-dev.sdevops99.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.cart.private_ip]
 }
 
 resource "aws_instance" "redis" {
-  ami           = "ami-03265a0778a880afb"
-  instance_type = "t3.small"
+  ami                    = ami-03265a0778a880afb
+  instance_type          = "t3.small"
+  vpc_security_group_ids = ["sg-041edd0364521caf3"]
 
   tags = {
     Name = "redis"
   }
 }
 
-resource "aws_instance" "mysql" {
-  ami           = "ami-03265a0778a880afb"
-  instance_type = "t3.small"
-
-  tags = {
-    Name = "mysql"
-  }
+resource "aws_route53_record" "redis" {
+  zone_id = Z0018290WARH7MQ8A27A
+  name    = "redis-dev.sdevops99.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.redis.private_ip]
 }
 
-resource "aws_instance" "shipping" {
-  ami           = "ami-03265a0778a880afb"
-  instance_type = "t3.small"
-
-  tags = {
-    Name = "shipping"
-  }
-}
-
-resource "aws_instance" "rabbitmq" {
-  ami           = "ami-03265a0778a880afb"
-  instance_type = "t3.small"
-
-  tags = {
-    Name = "rabbitmq"
-  }
-}
 
 resource "aws_instance" "user" {
-  ami           = "ami-03265a0778a880afb"
-  instance_type = "t3.small"
+  ami                    = ami-03265a0778a880afb
+  instance_type          = "t3.small"
+  vpc_security_group_ids = ["sg-041edd0364521caf3"]
 
   tags = {
     Name = "user"
   }
 }
 
+resource "aws_route53_record" "user" {
+  zone_id = Z0018290WARH7MQ8A27A
+  name    = "user-dev.sdevops99.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.user.private_ip]
+}
+
+resource "aws_instance" "mysql" {
+  ami                    = ami-03265a0778a880afb
+  instance_type          = "t3.small"
+  vpc_security_group_ids = ["sg-041edd0364521caf3"]
+
+  tags = {
+    Name = "mysql"
+  }
+}
+
+resource "aws_route53_record" "mysql" {
+  zone_id = Z0018290WARH7MQ8A27A
+  name    = "mysql-dev.sdevops99.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mysql.private_ip]
+}
+
+resource "aws_instance" "shipping" {
+  ami                    = ami-03265a0778a880afb
+  instance_type          = "t3.small"
+  vpc_security_group_ids = ["sg-041edd0364521caf3"]
+
+  tags = {
+    Name = "shipping"
+  }
+}
+
+resource "aws_route53_record" "shipping" {
+  zone_id = Z0018290WARH7MQ8A27A
+  name    = "shipping-dev.sdevops99.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.shipping.private_ip]
+}
+
+resource "aws_instance" "rabbitmq" {
+  ami                    = ami-03265a0778a880afb
+  instance_type          = "t3.small"
+  vpc_security_group_ids = ["sg-041edd0364521caf3"]
+
+  tags = {
+    Name = "rabbitmq"
+  }
+}
+
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = Z0018290WARH7MQ8A27A
+  name    = "rabbitmq-dev.sdevops99.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.rabbitmq.private_ip]
+}
+
 resource "aws_instance" "payment" {
-  ami           = "ami-03265a0778a880afb"
-  instance_type = "t3.small"
+  ami                    = ami-03265a0778a880afb
+  instance_type          = "t3.small"
+  vpc_security_group_ids = ["sg-041edd0364521caf3"]
 
   tags = {
     Name = "payment"
   }
 }
 
-resource "aws_instance" "dispatch" {
-  ami           = "ami-03265a0778a880afb"
-  instance_type = "t3.small"
-
-  tags = {
-    Name = "dispatch"
-  }
+resource "aws_route53_record" "payment" {
+  zone_id = Z0018290WARH7MQ8A27A
+  name    = "payment-dev.sdevopsb99.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.payment.private_ip]
 }
+
+
