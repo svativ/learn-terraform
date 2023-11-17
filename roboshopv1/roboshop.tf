b04a11b842d31fc1e67db4952881aeb332581c95
svati <svativangapandu9@gmail.com>
2023-11-17 16:10:27 +0530

    teraform

--- roboshopv1/roboshop.tf
+++ roboshopv1/roboshop.tf
@@ -178,3 +178,20 @@ resource "aws_route53_record" "payment" {
   ttl     = 30
   records = [aws_instance.payment.private_ip]
 }
+resource "aws_instance" "dispatch" {
+  ami                    = "ami-03265a0778a880afb"
+  instance_type          = "t3.small"
+  vpc_security_group_ids = ["sg-041edd0364521caf3"]
+
+  tags = {
+    Name = "payment"
+  }
+}
+
+resource "aws_route53_record" "dispatch" {
+  zone_id = "Z0018290WARH7MQ8A27A"
+  name    = "dispatch-dev.sdevops99.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.payment.private_ip]
+}
