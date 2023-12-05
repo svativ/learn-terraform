561734f1fd45cab98b196e405af936f4482f5f22
svati <svativangapandu9@gmail.com>
2023-12-05 21:40:37 +0530

    teraform

--- 0.3vars/sample.tf
+++ 0.3vars/sample.tf
@@ -1,8 +1,36 @@
-/plainvariable
+*/plainvariable
 variable "fruit_name" {
    default="apple"
 }
 
 output "fruit_name"{
     value=var.fruit_name
+}
+/*list variables*/
+variable "fruits" {
+   default = [
+     "apple"
+      "banana"
+   ]
+   /* default = [ "apple", "banana"] // single line syntax
+}
+/*map variable, map of maps*/
+variable "fruit_stock_with_price"{
+   default = {
+   apple = 100
+   banana = 200
+  }
+}
+/* map variable, map of maps*/
+variable "fruit_stock_with_price"{
+   default = {
+    apple = {
+       stock = 100
+       price = 3
+    }
+    banana = {
+      stock = 400
+       price = 1
+    }
+  }
 }
