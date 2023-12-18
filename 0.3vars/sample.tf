e5302b4d8e9bd75792b1e3d0e52da9f0dfee30cb
svati <svativangapandu9@gmail.com>
2023-12-18 09:12:01 +0530

    teraform

--- 0.3vars/sample.tf
+++ 0.3vars/sample.tf
@@ -12,30 +12,3 @@ variable "fruits" {
    default = [
      "apple"
       "banana"
-   ]
-   /* default = [ "apple", "banana"] // single line syntax
-}
-
-/*
-map variable, map of maps
-*/
-variable "fruit_stock_with_price"{
-   default = {
-   apple = 100
-   banana = 200
-  }
-}
-
-/* map variable, map of maps*/
-  variable "fruit_stock_with_price"{
-    default = {
-     apple = {
-     stock = 100
-     price = 3
-}
-     banana = {
-     stock = 400
-     price = 1
-    }
-  }
-}
