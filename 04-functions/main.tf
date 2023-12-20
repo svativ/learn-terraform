0815fb431f73f2a9e3044b69fdb2d5c0751452d6
svati <svativangapandu9@gmail.com>
2023-12-20 08:57:42 +0530

    terraform

--- 04-functions/main.tf
+++ 04-functions/main.tf
@@ -16,3 +16,10 @@ variable "fruit_with_stock" {
 output "fruit_stock" {
   value =  try(var.fruit_with_stock["banana"],0)
 }
+
+
+
+output "fruit_stock_price"{
+  value = lookup(var.fruit_with_stock["apple"],"price",2)
+  /*try(var.fruit_with_stock["apple"].price,2*/
+}
