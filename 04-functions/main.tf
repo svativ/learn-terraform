38b05b89ee0ad94b46756537c5e248a1e9d6fad1
svati <svativangapandu9@gmail.com>
2023-12-20 08:11:51 +0530

    terraform

--- 04-functions/main.tf
+++ 04-functions/main.tf
@@ -6,3 +6,13 @@ output "fruits" {
   /*value = var.fruits[2]*/
   value = element(var.fruits,2)
 }
+
+variable "fruit_with_stock" {
+  default = {
+    apple = 100
+  }
+}
+
+output "fruit_stock" {
+  value = var.fruit_with_stock["banana"]
+}
