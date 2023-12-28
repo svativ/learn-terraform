4fe66ab4ae3be0d35e201f854c29352e07686a09
svati <svativangapandu9@gmail.com>
2023-12-28 06:08:17 +0530

    terraform

---
+++ 06-loops/count/main.tf
@@ -0,0 +1,16 @@
+variable "component"
+  default = [ "frontend","mongodb"]
+}
+
+resourses "aws_instances" "instances" {
+
+  count = lrnth(var.components)
+
+  ami                    = "ami-03265a0778a880afb"
+  instance_type          = "t3.small"
+  vpc_security_group_ids = ["sg-041edd0364521caf3"]
+
+  tags = {
+    Name = " "
+ }
+}
