04d6005ff139dde19452777d02753d37b397b0a2
svati <svativangapandu9@gmail.com>
2023-12-28 09:11:35 +0530

    terraform

--- roboshop-v2/main.tf
+++ roboshop-v2/main.tf
@@ -30,25 +30,27 @@ variable "components" {
 }
 
 resource "aws_instance" "instance" {
-  for_each = var.components
+  for_each               = var.components
   ami                    = var.ami
   instance_type          = var.instance_type
   vpc_security_group_ids = var.security_groups
 
   tags = {
-    Name = lookup (each.value, "name", null)
+    Name = lookup(each.value, "name", null)
   }
 }
 
 resource "aws_route53_record" "record" {
   zone_id = var.zone_id
   for_each = var.components
-  name    = "$lookup(each.value,"name",null)}.sdevops99.online"
-  type    = "A"
-  ttl     = 30
-  records = [lookup(lookup(aws_instance.instance,each.key,null),"private_ip",null]
+  name     = "$lookup(each.value, "name", null)}.sdevops99.online"
+  type     = "A"
+  ttl      = 30
+  records  = [lookup(lookup(aws_instance.instance, each.key, null), "private_ip",null)]
 
 }
+
+
 
 #output "instances" {
 #  value = aws_instance.instance
