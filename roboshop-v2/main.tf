1db97c55fde94907491bf14a1a97978d33994aec
svati <svativangapandu9@gmail.com>
2023-12-28 10:09:53 +0530

    terraform

--- roboshop-v2/main.tf
+++ roboshop-v2/main.tf
@@ -38,6 +38,7 @@ resource "aws_instance" "instance" {
   instance_type          = var.instance_type
   vpc_security_group_ids = var.security_groups
 
+
   tags = {
     Name = lookup(each.value, "name", null)
   }
