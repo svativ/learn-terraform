c15cb454c8bfa1180b81c5902425ec91df961d61
svati <svativangapandu9@gmail.com>
2023-12-28 08:03:00 +0530

    terraform

--- 06-loops/for each/main.tf
+++ 06-loops/for each/main.tf
@@ -1,29 +1,16 @@
+
+
 variable "components" {
-default = {
-  catalogue = { name = "catalogue-sg" }
-  mongodb    = { name =  "mongodb-sg" }
+  default = {
+    frontend  = { name = "frontend_sg" }
+    catalogue = { name = "catalogue_sg" }
+    mongodb   = { name = "mongodb_sg" }
   }
 }
 
 
-
 resource "aws_security_group" "allow_tls" {
   for_each = var.components
-  name       lookup(var.components,each.value["name"], null)
-
+  name     = lookup(var.components, each.value["name"], null)
 }
-#
-#variable "components" {
-#  default = {
-#    frontend  = { name = "frontend_sg" }
-#    catalogue = { name = "catalogue_sg" }
-#    mongodb   = { name = "mongodb_sg" }
-#  }
-#}
-#
-#
-#resource "aws_security_group" "allow_tls" {
-#  for_each = var.components
-#  name     = lookup(var.components, each.value["name"], null)
-#}
-#
+
