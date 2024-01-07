29155321da8326bd90170992a64c27738766a358
svati <svativangapandu9@gmail.com>
2024-01-07 07:42:38 +0530

    terraform

--- 07.module/local-module/data.tf
+++ 07.module/local-module/data.tf
@@ -12,7 +12,7 @@ data "aws_ami" "ami" {
 resource "aws_instance" "instance" {
   ami                    = data.aws_ami.ami.id
   instance_type          = "t3.small"
-  vpc_security_group_ids = ["sg-0dee954b08055e577"]
+  vpc_security_group_ids = ["sg-041edd0364521caf3"]
 
 }
 
