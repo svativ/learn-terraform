d25c70f263f4dc276051fd06fa663975bb8fea61
svati <svativangapandu9@gmail.com>
2024-01-07 12:42:43 +0530

    terraform

---
+++ roboshop-v3/vars.tf
@@ -0,0 +1,15 @@
+variable "security_groups" {
+  default = ["sg-041edd0364521caf3"]
+
+variable "zone_id" {
+  default = "Z0018290WARH7MQ8A27A"
+}
+
+variable "components" {
+  default = {
+    frontend = {
+      name = "frontend"
+      instance_type = "t3.micro"
+    }
+  }
+}
