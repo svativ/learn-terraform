f6da8c22fcc51e77ce1ee19f3e2f76501f82972f
svati <svativangapandu9@gmail.com>
2024-01-07 12:33:45 +0530

    terraform

--- roboshop-v3/main.tf
+++ roboshop-v3/main.tf
@@ -1,3 +1,11 @@
 module "components" {
-  source = "./module"
+
+  for_each = var.components
+
+  source            = "./module"
+  zone_id           = var.zone_id
+  security_groups   = var.security_groups
+  name              = each.value["name"]
+  instance_type     = each.value["instance_type"]
+
 }
