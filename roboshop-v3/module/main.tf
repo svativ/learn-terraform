1a5cd109f161f001abc5fe3cefa1f1be80894a94
svati <svativangapandu9@gmail.com>
2024-01-08 08:45:12 +0530

    terraform

--- roboshop-v3/module/main.tf
+++ roboshop-v3/module/main.tf
@@ -1,5 +1,4 @@
 resource "aws_instance" "instance" {
-
   ami                    = data.aws_ami.ami.id
   instance_type          = var.instance_type
   vpc_security_group_ids = var.security_groups
@@ -8,8 +7,11 @@ resource "aws_instance" "instance" {
   tags = {
     Name = var.name
   }
-}
 
+  provisioner "local-exec" {
+    command = "sleep 60"
+  }
+}
 resource "aws_route53_record" "record" {
 
   zone_id  = var.zone_id
@@ -19,10 +21,3 @@ resource "aws_route53_record" "record" {
   records  = [aws_instance.instance.private_ip]
 }
 
-/*resource "aws_route53_record" "record" {
-  zone_id = var.zone_id
-  name    = "${var.name}-dev.rdevopsb72.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.instance.private_ip]
-}*/
