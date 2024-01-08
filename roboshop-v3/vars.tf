09afe3bcabc84a53be98818004c2b5bdd92e3230
svati <svativangapandu9@gmail.com>
2024-01-08 12:48:49 +0530

    terraform

--- roboshop-v3/vars.tf
+++ roboshop-v3/vars.tf
@@ -6,15 +6,49 @@ variable "zone_id" {
   default = "Z0018290WARH7MQ8A27A"
 }
 
+
+
+
+
 variable "components" {
-  default = {
-    frontend = {
-      name = "frontend"
-      instance_type = "t3.micro"
-    }
-    cart = {
-      name = "cart"
-     instance_type = "t3.micro"
-    }
-  }
+default = {
+frontend = {
+name          = "frontend"
+instance_type = "t3.micro"
 }
+cart = {
+name          = "cart"
+instance_type = "t3.micro"
+}
+mongodb = {
+name          = "mongodb"
+instance_type = "t3.micro"
+}
+catalogue = {
+name          = "catalogue"
+instance_type = "t3.micro"
+}
+user = {
+name          = "user"
+instance_type = "t3.micro"
+}
+redis = {
+name          = "redis"
+instance_type = "t3.micro"
+}
+mysql = {
+name          = "mysql"
+instance_type = "t3.micro"
+}
+payment = {
+name          = "payment"
+instance_type = "t3.micro"
+}
+shipping = {
+name          = "shipping"
+instance_type = "t3.micro"
+}
+rabbitmq = {
+name          = "rabbitmq"
+instance_type = "t3.micro"
+}\
