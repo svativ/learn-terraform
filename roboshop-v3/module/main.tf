f2392ea74b1e8d5a17ae666f1ae27b93911c1249
svati <svativangapandu9@gmail.com>
2024-01-08 12:26:07 +0530

    terraform

--- roboshop-v3/module/main.tf
+++ roboshop-v3/module/main.tf
@@ -17,17 +17,19 @@ resource "aws_route53_record" "record" {
   ttl      = 30
   records  = [aws_instance.instance.private_ip]
 }
-resource "null_resource" "ansible" {}
 
-  depends_on = [
-    aws_route53_record.record
-  ]
-provisioner "local-exec"
-   commond = <<EOF
+source "null_resource" "ansible" {
+
+depends_on = [
+aws_route53_record.record
+]
+
+provisioner "local-exec" {
+command = <<EOF
 cd /home/centos/roboshop-ansible
 git pull
 sleep 30
-ansible-playbook -i ${var.name}-devsdevops99.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component = ${var.name}
+ansible-playbook -i ${var.name}-dev.rdevopsb72.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component=${var.name}
 EOF
- }
+}
 }
